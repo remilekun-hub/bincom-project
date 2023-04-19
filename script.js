969992b15b1e07789918f642b0d2f8ff509b0571
Atanda remilekun <atandaremilekun@gmail.com>
2023-04-19 18:43:29 +0100

    ready

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const btn = document.querySelector("#btn");
 const mobilemenu = document.querySelector("#mobile-menu");
 const closebtn = document.querySelector(".close-btn");
 const sectionLink = document.querySelectorAll(".section-link");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 
 btn.addEventListener("click", () => mobilemenu.classList.add("showmenu"));
 
@@ -14,3 +16,44 @@ sectionLink.forEach((link) =>
     mobilemenu.classList.remove("showmenu");
   })
 );
+
+prev.addEventListener("click", () => {
+  plusSlides(-1);
+});
+
+next.addEventListener("click", () => {
+  plusSlides(1);
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
